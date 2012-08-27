a7a45504a2521572ac96c6d1429464791e51aaaa
Louis Delacroix <yoursecretsaresafe@googlemail.com>
2012-08-27 20:51:15 +0100

    Added license (Boost License)

--- prettyprint.hpp
+++ prettyprint.hpp
@@ -1,3 +1,9 @@
+//          Copyright Louis Delacroix 2010 - 2012.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
 #ifndef H_PRETTY_PRINT
 #define H_PRETTY_PRINT
 
